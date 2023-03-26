5275fea3f776db83c5324a01d7602a0f19502b08
MarsX-2002 <mirjalol0331@gmail.com>
2023-03-26 09:53:06 +0500

    data types, type casting, logical operators, digits of a number

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+//        1234
+        int fourth = number % 10;
+        int third = (number % 100 - fourth) / 10;
+        int second = (number % 1000 - third * 10 - fourth) / 100;
+        int first = number / 1000;
+
+        int sumOfDigits = first + second + third + fourth;
+        System.out.println(sumOfDigits);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int third = number % 10;
+        int second = (number % 100 - third) / 10;
+        int first = number / 100;
+        int reverted = third * 100 + second * 10 + first;
+
+        System.out.println(reverted);
     }
 }
